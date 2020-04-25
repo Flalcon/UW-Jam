aa05bf788b63e69b0a175fd83a2a5c3ac9f40a6f
Flalcon <42194518+Flalcon@users.noreply.github.com>
2020-04-24 19:43:59 -0700

    Start of Project

---
+++ touhou/Dreams/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public bool canMove = true;
+    public float hsp = 0; public float vsp = 0;
+    public float spd = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove) {
+           Movement();
+        }
+
+        bool shoot = Input.GetKey("n") || Input.GetKey(KeyCode.Space);
+        if (shoot) {
+            
+
+        }
+
+
+    }
+
+    public void Movement() {
+        bool up = Input.GetKey(("w")) || Input.GetKey(KeyCode.UpArrow);
+        bool down = Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow);
+        bool left = Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow);
+        bool right = Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow);
+        
+       // bool bomb = Input.GetKey("n") || Input.GetKey(KeyCode.Space);
+        if (up)   { vsp += spd; }
+        if (down) { vsp -= spd; }
+        if (left) { hsp -= spd; }
+        if (right) {hsp += spd; }
+
+        hsp = Mathf.Clamp(hsp, -1, 1);
+        vsp = Mathf.Clamp(vsp, -2, 2);
+
+        if (up == down) {
+            if (Mathf.Abs(vsp) > 0.2)
+            {
+                vsp *= 0.9f;
+            }
+            else { vsp = 0; }
+        }
+
+        if (right == left)
+        {
+            if (Mathf.Abs(hsp) > 0.2)
+            {
+                hsp *= 0.9f;
+            }
+            else { hsp = 0; }
+        }
+
+        gameObject.transform.Translate(new Vector3(hsp * Time.deltaTime,vsp * Time.deltaTime,0));
+    }
+
+
+}
