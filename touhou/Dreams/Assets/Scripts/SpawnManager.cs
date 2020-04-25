132f5cda14734abf4868e2682b0693d12fb685b7
otool047 <otool047@gmail.com>
2020-04-25 09:34:14 -0700

    Update to spawn manager
    Updated the spawn manager to spawn enemies at the edge of the screen and at a random y position.

--- touhou/Dreams/Assets/Scripts/SpawnManager.cs
+++ touhou/Dreams/Assets/Scripts/SpawnManager.cs
@@ -7,6 +7,8 @@ public class SpawnManager : MonoBehaviour
     public float secondsBetweenSpawn;
     public float elapsedTime = 0.0f;
     public GameObject enemyObject;
+    public float xSpawnPos = 11.0f;
+    public float ySpawnRange = 4.5f; 
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +25,14 @@ public class SpawnManager : MonoBehaviour
         {
             elapsedTime = 0;
 
-            Vector2 spawnPosition = new Vector2(0, 0);
+            Vector2 spawnPosition = GenerateSpawnPosition();
             GameObject newEnemy = Instantiate(enemyObject, spawnPosition, enemyObject.transform.rotation);
         }
+    }
+
+    Vector2 GenerateSpawnPosition()
+    {
+        float yPos = Random.Range(-ySpawnRange, ySpawnRange);
+        return new Vector2(xSpawnPos, yPos);
     }
 }
