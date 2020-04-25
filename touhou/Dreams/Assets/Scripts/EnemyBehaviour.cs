dc2c9aed5750ea6910e9c2dade8a12b9eb3b3a3f
Flalcon <42194518+Flalcon@users.noreply.github.com>
2020-04-24 20:36:33 -0700

    Added Enemy & bullets

---
+++ touhou/Dreams/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public GameObject EB;
+    public float BT;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        //move towards player
+        //shoots bullets
+        gameObject.transform.Translate(new Vector3(-0.5f * Time.deltaTime,0,0));
+        if (BT <= 0) {
+            GameObject B = Instantiate(EB);
+            B.transform.SetPositionAndRotation(gameObject.transform.position, new Quaternion());
+            BT = 2;
+        }
+        BT -= Time.deltaTime;
+        }
+}
---
+++ touhou/Dreams/Assets/Scripts/EnemyBullets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullets : MonoBehaviour
+{
+    void Update()
+    {
+            if (gameObject.transform.position.x >= -10)
+            {
+                gameObject.transform.Translate(new Vector3(-2f * Time.deltaTime, 0, 0));
+            }
+            else
+            {
+                //make a pool of objects set up instead of destroying each object
+                Destroy(gameObject);
+            }
+
+    }
+}
