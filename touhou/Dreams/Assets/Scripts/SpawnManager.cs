1435499ec74bb127226e0688603c5a039de19497
otool047 <otool047@gmail.com>
2020-04-25 09:11:29 -0700

    Created basic spawn manager to consistently spawn new enemies

---
+++ touhou/Dreams/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public float secondsBetweenSpawn;
+    public float elapsedTime = 0.0f;
+    public GameObject enemyObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > secondsBetweenSpawn)
+        {
+            elapsedTime = 0;
+
+            Vector2 spawnPosition = new Vector2(0, 0);
+            GameObject newEnemy = Instantiate(enemyObject, spawnPosition, enemyObject.transform.rotation);
+        }
+    }
+}
