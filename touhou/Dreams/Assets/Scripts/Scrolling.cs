e07cfc343dd6c8b52e77c48b0c874da01b656cb7
Flalcon <42194518+Flalcon@users.noreply.github.com>
2020-04-25 21:08:59 -0700

    I guess sample scene time

--- touhou/Dreams/Assets/Scripts/Scrolling.cs
+++ touhou/Dreams/Assets/Scripts/Scrolling.cs
@@ -13,36 +13,36 @@ public class Scrolling : MonoBehaviour
     
     void Update()
     {
-        BG1.transform.Translate(-spd * 0.4f * Time.deltaTime,0,0);
+        BG1.transform.Translate(-spd * 6f * Time.deltaTime,0,0);
         if(BG1_1.transform.position.x <= 0){
             BG1.transform.SetPositionAndRotation(new Vector3(0,1,0), new Quaternion());
         }
 
-        BG2.transform.Translate(-spd * 0.6f * Time.deltaTime, 0, 0);
+        BG2.transform.Translate(-spd * 5f * Time.deltaTime, 0, 0);
         if (BG2_1.transform.position.x <= 0)
         {
             BG2.transform.SetPositionAndRotation(new Vector3(0, 1, 0), new Quaternion());
         }
 
-        BG3.transform.Translate(-spd * 1.8f * Time.deltaTime, 0, 0);
+        BG3.transform.Translate(-spd * 4f * Time.deltaTime, 0, 0);
         if (BG3_1.transform.position.x <= 0)
         {
             BG3.transform.SetPositionAndRotation(new Vector3(0, 1, 0), new Quaternion());
         }
 
-        BG4.transform.Translate(-spd * 2.4f * Time.deltaTime, 0, 0);
+        BG4.transform.Translate(-spd * 3f * Time.deltaTime, 0, 0);
         if (BG4_1.transform.position.x <= 0)
         {
             BG4.transform.SetPositionAndRotation(new Vector3(0, 1, 0), new Quaternion());
         }
 
-        BG5.transform.Translate(-spd * 4f * Time.deltaTime, 0, 0);
+        BG5.transform.Translate(-spd * 2f * Time.deltaTime, 0, 0);
         if (BG5_1.transform.position.x <= 0)
         {
             BG5.transform.SetPositionAndRotation(new Vector3(0, 1, 0), new Quaternion());
         }
 
-        BG6.transform.Translate(-spd * 6f * Time.deltaTime, 0, 0);
+        BG6.transform.Translate(-spd * 1f * Time.deltaTime, 0, 0);
         if (BG6_1.transform.position.x <= 0)
         {
             BG6.transform.SetPositionAndRotation(new Vector3(0, 1, 0), new Quaternion());
