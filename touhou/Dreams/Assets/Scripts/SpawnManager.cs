cfbf3c75d33ff1af8ea4ce5d6cf0556b085670b6
Flalcon <42194518+Flalcon@users.noreply.github.com>
2020-04-25 18:27:22 -0700

    Win Screen And Levels

--- touhou/Dreams/Assets/Scripts/SpawnManager.cs
+++ touhou/Dreams/Assets/Scripts/SpawnManager.cs
@@ -7,21 +7,25 @@ public class SpawnManager : MonoBehaviour
     public float secondsBetweenSpawn;
     public float elapsedTime = 0.0f;
     public GameObject enemyObject;
+    public GameObject playerObject;
     public float xSpawnPos = 10.0f;
     public float ySpawnRange = 4.5f;
     private int level = 1;
     public int tolevel = 3;
+    private int maxlevel;
+    private bool spawning = true;
+    public GameObject winScreen;
     
     void Start()
     {
-        
+        maxlevel = 5;
     }
     
     void Update()
     {
         elapsedTime += Time.deltaTime;
 
-        if (elapsedTime > secondsBetweenSpawn)
+        if (elapsedTime > secondsBetweenSpawn && spawning)
         {
             elapsedTime = 0;
 
@@ -30,10 +34,15 @@ public class SpawnManager : MonoBehaviour
             int type = Mathf.RoundToInt(Random.Range(0,level));
             newEnemy.GetComponent<EnemyBehaviour>().type = type;//Implement something so we can set spawn positions later(only do this if we have time)
             //if (the player has killed x many enemies) {level up, secondsBetweenSpawn gets smaller}
-            if (GetComponent<PlayerMovement>().score > tolevel) {
-
-
-
+            if (playerObject.GetComponent<PlayerMovement>().score >= tolevel) {
+                playerObject.GetComponent<PlayerMovement>().score = 0;
+                tolevel += 2;
+                level++;
+                secondsBetweenSpawn--;
+                if (level >= maxlevel) {
+                    spawning = false;
+                    winScreen.SetActive(true);
+                }
             }
                
         }
