01dda90ca6895db7434d3b86d0c29c0d81f168ba
otool047 <otool047@gmail.com>
2020-04-26 18:48:53 -0700

    big player wave destroys
    big player wave now destroys any triggers or collisions that interact with it, effectively killing all enemies and their bullets

--- touhou/Dreams/Assets/Scripts/BigPlayerWave.cs
+++ touhou/Dreams/Assets/Scripts/BigPlayerWave.cs
@@ -13,7 +13,7 @@ public class BigPlayerWave : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.transform.position.x <= 10)
+        if (gameObject.transform.position.x <= 15)
         {
             gameObject.transform.Translate(new Vector3(10f * Time.deltaTime, 0, 0));
         }
@@ -21,6 +21,22 @@ public class BigPlayerWave : MonoBehaviour
         {
             //make a pool of objects set up instead of destroying each object
             Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("big player wave used");
+        } else
+        {
+            Destroy(collision.gameObject);
         }
     }
 }
