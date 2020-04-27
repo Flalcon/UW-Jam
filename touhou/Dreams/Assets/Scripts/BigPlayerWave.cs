bc714307f9671029c0e8342f4f4ef36c988b997a
otool047 <otool047@gmail.com>
2020-04-26 18:29:23 -0700

    big player wave behavior
    giving the big player wave movement and will destroy at the right edge

---
+++ touhou/Dreams/Assets/Scripts/BigPlayerWave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigPlayerWave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.x <= 10)
+        {
+            gameObject.transform.Translate(new Vector3(10f * Time.deltaTime, 0, 0));
+        }
+        else
+        {
+            //make a pool of objects set up instead of destroying each object
+            Destroy(gameObject);
+        }
+    }
+}
