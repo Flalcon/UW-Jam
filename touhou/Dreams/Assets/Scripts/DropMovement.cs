d9069d7541877b50b9c9a8a8111318bf771bbbf6
Flalcon <42194518+Flalcon@users.noreply.github.com>
2020-04-27 15:45:10 -0700

    Made Drops move to the left

---
+++ touhou/Dreams/Assets/Scripts/DropMovement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropMovement : MonoBehaviour
+{
+    void Update()
+    {
+        gameObject.transform.Translate(-3 * Time.deltaTime,0,0);
+    }
+}
