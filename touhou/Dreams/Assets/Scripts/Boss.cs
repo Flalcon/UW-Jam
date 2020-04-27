d0df3d5cd311c50c023ac8dffabd89ec36d5669d
otool047 <otool047@gmail.com>
2020-04-26 21:50:03 -0700

    Add boss focal point
    Adding a focal point which will be the origin of the boss attacks

--- touhou/Dreams/Assets/Scripts/Boss.cs
+++ touhou/Dreams/Assets/Scripts/Boss.cs
@@ -8,6 +8,7 @@ public class Boss : MonoBehaviour
     public float speed = 2.0f;
     public GameObject EB;
     public float bulletspd = 3.0f;
+    public GameObject focalPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +33,7 @@ public class Boss : MonoBehaviour
     private void fireBullets1()
     {
         GameObject B = Instantiate(EB);
-        B.transform.SetPositionAndRotation(gameObject.transform.position, new Quaternion());
+        B.transform.SetPositionAndRotation(focalPoint.gameObject.transform.position, new Quaternion());
         B.GetComponent<EnemyBullets>().dir = new Vector2(bulletspd, Random.Range(-bulletspd, bulletspd));
     }
 }
