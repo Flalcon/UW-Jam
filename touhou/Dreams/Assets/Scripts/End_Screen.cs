295a1b96e27c382d0af65196087c8c0fe1bd853f
Flalcon <42194518+Flalcon@users.noreply.github.com>
2020-04-28 01:35:07 -0700

    Added Win Scene

---
+++ touhou/Dreams/Assets/Scripts/End_Screen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End_Screen : MonoBehaviour
+{
+    private float timer;
+    private bool s;
+
+    void Start()
+    {
+        s = false;
+        timer = 0;
+    }
+
+    void Update()
+    {
+        if (s)
+        {
+            GetComponent<AudioSource>().volume = timer;
+            timer -= 0.2f * Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                SceneManager.LoadScene(0);
+
+            }
+        }
+        else {
+            timer += 0.2f * Time.deltaTime;
+            timer = Mathf.Clamp(timer, 0, 1);
+        }
+
+
+        if (Input.anyKeyDown)
+        {
+            s = true;
+        }
+
+        GetComponent<SpriteRenderer>().color = new Color(timer, timer, timer, 1);
+    }
+}
---
+++ touhou/Dreams/Assets/Scripts/WinScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    private float timer;
+    public GameObject aud;
+
+    void Start()
+    {
+        timer = 1;
+    }
+
+    void Update()
+    {
+        aud.GetComponent<AudioSource>().volume = timer;
+        GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, 1-timer);
+
+        if (Input.anyKeyDown && timer <= 0)
+        {
+            SceneManager.LoadScene(3);
+        }
+        timer -= 0.2f * Time.deltaTime;
+    }
+}
